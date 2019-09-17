90ae997a946f85bbab9d717ace2b63a23d88522b
Geraldine Lustgarten <geraldinelustgarten@Geraldines-MacBook.local>
2019-09-17 14:52:46 -0500

    Imported doubly linked list into lru cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,5 @@
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
   """
   Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +9,11 @@ class LRUCache:
   to every node stored in the cache.
   """
   def __init__(self, limit=10):
-    pass
+    self.limit = limit
+    self.size = 0
+    self.dll = 
+    self.storage = {}
+
 
   """
   Retrieves the value associated with the given key. Also
